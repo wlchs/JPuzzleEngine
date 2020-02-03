dea8cde06b16744457cbe602630ff200ebd0ed0f
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-03 20:38:10 +0100

    set stored output matrix in single output handler

--- src/main/java/com/laszloborbely/jsudooku/core/io/AbstractSingleOutput.java
+++ src/main/java/com/laszloborbely/jsudooku/core/io/AbstractSingleOutput.java
@@ -1,8 +1,16 @@
 package com.laszloborbely.jsudooku.core.io;
 
+import com.laszloborbely.jsudooku.core.matrix.IMatrix;
+
 public abstract class AbstractSingleOutput implements ISudokuOutput {
+    protected IMatrix outputMatrix;
+
     @Override
     public final boolean many() {
         return false;
+    }
+
+    public final void setSingleOutput(IMatrix output) {
+        this.outputMatrix = output;
     }
 }
