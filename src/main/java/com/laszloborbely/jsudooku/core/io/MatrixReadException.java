68c3d9a41ca9bc204ce8d07b72bcc716ef21c095
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-03 17:04:00 +0100

    create new exception type for matrix read errors

---
+++ src/main/java/com/laszloborbely/jsudooku/core/io/MatrixReadException.java
@@ -0,0 +1,7 @@
+package com.laszloborbely.jsudooku.core.io;
+
+public class MatrixReadException extends Exception {
+    public MatrixReadException() {
+        super("Matrix read error!");
+    }
+}
