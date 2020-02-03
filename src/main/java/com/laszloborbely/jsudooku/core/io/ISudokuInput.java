8e62ffa8e3235dd0ecf2fba8582dae9b795a051a
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-03 20:37:43 +0100

    bind reducer to input

--- src/main/java/com/laszloborbely/jsudooku/core/io/ISudokuInput.java
+++ src/main/java/com/laszloborbely/jsudooku/core/io/ISudokuInput.java
@@ -1,6 +1,9 @@
 package com.laszloborbely.jsudooku.core.io;
 
+import com.laszloborbely.jsudooku.core.matrix.IMatrixReducer;
+
 public interface ISudokuInput {
     void read();
     boolean many();
+    IMatrixReducer getReducer();
 }
