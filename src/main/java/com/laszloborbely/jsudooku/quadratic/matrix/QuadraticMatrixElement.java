46e51655a5a8a9a56a70fee95a3c4ad055147ac3
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-03 23:04:37 +0100

    implement QuadraticMatrixElement

--- src/main/java/com/laszloborbely/jsudooku/quadratic/matrix/QuadraticMatrixElement.java
+++ src/main/java/com/laszloborbely/jsudooku/quadratic/matrix/QuadraticMatrixElement.java
@@ -2,6 +2,44 @@ package com.laszloborbely.jsudooku.quadratic.matrix;
 
 import com.laszloborbely.jsudooku.core.matrix.IMatrixElement;
 
+/**
+ * Simple integer (short) element of a quadratic puzzle
+ */
 public class QuadraticMatrixElement implements IMatrixElement {
 
+    /**
+     * Private short member variable storing actual value
+     */
+    private Short value;
+
+    /**
+     * Empty constructor defining non-fixed matrix values
+     */
+    QuadraticMatrixElement() {
+
+    }
+
+    /**
+     * Constructor for fixed matrix element
+     * @param value Member value
+     */
+    QuadraticMatrixElement(Short value) {
+        this.value = value;
+    }
+
+    /**
+     * Member value getter function
+     * @return Short member value of field
+     */
+    public Short getValue() {
+        return this.value;
+    }
+
+    /**
+     * Member value setter function
+     * @param value Short value
+     */
+    public void setValue(Short value) {
+        this.value = value;
+    }
 }
