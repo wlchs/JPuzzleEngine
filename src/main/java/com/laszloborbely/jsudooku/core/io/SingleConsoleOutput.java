b95ff237f3ed7817853188ab9162086b31654324
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-03 17:02:03 +0100

    remove dummy main implementation

--- src/main/java/com/laszloborbely/jsudooku/core/io/SingleConsoleOutput.java
+++
@@ -1,7 +0,0 @@
-package com.laszloborbely.jsudooku.core.io;
-
-public final class SingleConsoleOutput extends AbstractSingleOutput {
-    public void write() {
-
-    }
-}
--- src/main/java/com/laszloborbely/jsudooku/core/io/SingleConstInput.java
+++
@@ -1,7 +0,0 @@
-package com.laszloborbely.jsudooku.core.io;
-
-public final class SingleConstInput extends AbstractSingleInput {
-    public void read() {
-
-    }
-}
