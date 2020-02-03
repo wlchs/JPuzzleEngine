cba8e2dec832290a797581adf5016501be6456af
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-03 17:05:32 +0100

    abstract final matrix input getter functions

--- src/main/java/com/laszloborbely/jsudooku/core/io/AbstractMultiInput.java
+++ src/main/java/com/laszloborbely/jsudooku/core/io/AbstractMultiInput.java
@@ -1,8 +1,22 @@
 package com.laszloborbely.jsudooku.core.io;
 
+import com.laszloborbely.jsudooku.core.matrix.IMatrix;
+
 public abstract class AbstractMultiInput implements ISudokuInput {
+    protected IMatrix inputMatrix;
+
     @Override
     public final boolean many() {
         return true;
+    }
+
+    public final IMatrix getNextInput() throws MatrixReadException {
+        if (inputMatrix == null) {
+            this.read();
+        }
+
+        IMatrix next = this.inputMatrix;
+        this.inputMatrix = null;
+        return next;
     }
 }
--- src/main/java/com/laszloborbely/jsudooku/core/io/AbstractSingleInput.java
+++ src/main/java/com/laszloborbely/jsudooku/core/io/AbstractSingleInput.java
@@ -1,8 +1,19 @@
 package com.laszloborbely.jsudooku.core.io;
 
+import com.laszloborbely.jsudooku.core.matrix.IMatrix;
+
 public abstract class AbstractSingleInput implements ISudokuInput {
+    protected IMatrix inputMatrix;
+
     @Override
     public final boolean many() {
         return false;
+    }
+
+    public final IMatrix getSingleInput() throws MatrixReadException {
+        if (inputMatrix == null) {
+            this.read();
+        }
+        return this.inputMatrix;
     }
 }
