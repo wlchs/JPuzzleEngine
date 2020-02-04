cd93a2306c6df3eb31f6d2f618167bb05c8f2734
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-04 20:54:52 +0100

    output handler comments

--- src/main/java/com/laszloborbely/jsudooku/core/io/AbstractMultiOutput.java
+++ src/main/java/com/laszloborbely/jsudooku/core/io/AbstractMultiOutput.java
@@ -1,6 +1,14 @@
 package com.laszloborbely.jsudooku.core.io;
 
+/**
+ * Abstract class representing handler for multiple output puzzles
+ */
 public abstract class AbstractMultiOutput implements ISudokuOutput {
+    /**
+     * Boolean function indicating that there are multiple output objects available
+     *
+     * @return Always true
+     */
     @Override
     public final boolean many() {
         return true;
--- src/main/java/com/laszloborbely/jsudooku/core/io/ISudokuOutput.java
+++ src/main/java/com/laszloborbely/jsudooku/core/io/ISudokuOutput.java
@@ -1,6 +1,18 @@
 package com.laszloborbely.jsudooku.core.io;
 
+/**
+ * Interface of solution output handlers
+ */
 public interface ISudokuOutput {
+    /**
+     * Writer function of the solution matrix
+     */
     void write();
+
+    /**
+     * Boolean function indicating how many solutions are expected
+     *
+     * @return True if all of the solutions are wanted, false if only one
+     */
     boolean many();
 }
