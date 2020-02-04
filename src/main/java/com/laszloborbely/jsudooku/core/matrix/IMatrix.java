23a14cfa2d71be0380ef2b7d6263e7e61b928bbd
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-04 21:10:24 +0100

    extend iterable interface

--- src/main/java/com/laszloborbely/jsudooku/core/matrix/IMatrix.java
+++ src/main/java/com/laszloborbely/jsudooku/core/matrix/IMatrix.java
@@ -1,5 +1,15 @@
 package com.laszloborbely.jsudooku.core.matrix;
 
-public interface IMatrix {
+/**
+ * Interface of matrix puzzles
+ * Extends the iterable interface to iterate over the contained matrix elements
+ */
+public interface IMatrix extends Iterable<IMatrixElement> {
+    /**
+     * Element getter function
+     *
+     * @param index Position of the element according to a specific matrix index
+     * @return Contained element at the specific index
+     */
     IMatrixElement getElement(IMatrixIndex index);
 }
