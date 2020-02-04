a1d7d02dcb0cb964dec574297f412fa274c01f8c
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-04 20:52:26 +0100

    remove unused matrix read exception

--- src/main/java/com/laszloborbely/jsudooku/core/io/MatrixReadException.java
+++
@@ -1,7 +0,0 @@
-package com.laszloborbely.jsudooku.core.io;
-
-public class MatrixReadException extends Exception {
-    public MatrixReadException() {
-        super("Matrix read error!");
-    }
-}
