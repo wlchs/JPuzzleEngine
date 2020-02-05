dec05711530d97c8442e9c0603146e5f2b7a6bbd
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-05 11:51:34 +0100

    single input exhaustive search implementation

--- src/main/java/com/laszloborbely/jsudooku/core/strategy/SingleInputExhaustiveStrategy.java
+++ src/main/java/com/laszloborbely/jsudooku/core/strategy/SingleInputExhaustiveStrategy.java
@@ -2,11 +2,28 @@ package com.laszloborbely.jsudooku.core.strategy;
 
 import com.laszloborbely.jsudooku.core.io.ISudokuInput;
 import com.laszloborbely.jsudooku.core.io.ISudokuOutput;
+import com.laszloborbely.jsudooku.core.matrix.IMatrix;
+import com.laszloborbely.jsudooku.core.matrix.IMatrixReducer;
+import com.laszloborbely.jsudooku.core.matrix.MatrixPair;
+
+import java.util.Stack;
 
 /**
  * Solving strategy for finding every solution for one input puzzle
  */
 public class SingleInputExhaustiveStrategy implements IStrategy {
+    /**
+     * Input object for a single puzzle
+     * Contains exactly one IMatrix element and a reducer object containing matrix reduction rules
+     */
+    protected ISudokuInput input;
+
+    /**
+     * Output object for a single solution
+     * Exactly one solution matrix can be set
+     */
+    protected ISudokuOutput output;
+
     /**
      * Initialing constructor
      *
@@ -14,9 +31,8 @@ public class SingleInputExhaustiveStrategy implements IStrategy {
      * @param output Output handler
      */
     public SingleInputExhaustiveStrategy(ISudokuInput input, ISudokuOutput output) {
-        /*
-         * TODO: initialize this
-         */
+        this.input = input;
+        this.output = output;
     }
 
     /**
@@ -26,7 +42,75 @@ public class SingleInputExhaustiveStrategy implements IStrategy {
      */
     public void execute() {
         /*
-         * TODO: implement this
+         * Get input matrix from input handler
          */
+        IMatrix inputMatrix = this.input.read();
+
+        /*
+         * Get reducer object from input handler
+         */
+        IMatrixReducer reducer = this.input.getReducer();
+
+        /*
+         * Initialize matrix splitter stack
+         * When a decision must be made, both options are pushed here
+         */
+        Stack<IMatrix> matrixStack = new Stack<>();
+
+        /*
+         * Initialize matrix stack with the input matrix
+         */
+        matrixStack.push(inputMatrix);
+
+        /*
+         * Execution runs until a solution is found or the matrix stack is empty
+         * The second case also means that the puzzle is unsolvable
+         */
+        while (!matrixStack.empty()) {
+
+            /*
+             * Retrieve top element of the stack
+             */
+            IMatrix matrix = matrixStack.pop();
+
+            /*
+             * Write intermediate state
+             */
+            /* output.writeIntermediate(matrix); */
+
+            /*
+             * Check if the top element is valid
+             */
+            if (reducer.validate(matrix)) {
+
+                /*
+                 * Check if the top element is a solution
+                 * If yes, write output
+                 */
+                if (reducer.solution(matrix)) {
+                    output.write(matrix);
+
+                } else {
+                    /*
+                     * Split solution space into two
+                     */
+                    MatrixPair split = reducer.split(matrix);
+
+                    /*
+                     * Check first matrix, if not null, push to stack
+                     */
+                    if (split.getFirst() != null) {
+                        matrixStack.push(split.getFirst());
+                    }
+
+                    /*
+                     * Check second matrix, if not null, push to stack
+                     */
+                    if (split.getSecond() != null) {
+                        matrixStack.push(split.getSecond());
+                    }
+                }
+            }
+        }
     }
 }
