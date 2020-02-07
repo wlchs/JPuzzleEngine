7d2ea8853aaf8b63cf23c1671ef4c28af25ad75c
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-02-07 20:59:19 +0100

    row and group reduction strategies

---
+++ src/main/java/com/laszloborbely/jpuzzle/sudoku/rules/solution/strategy/QGroupReductionStrategy.java
@@ -0,0 +1,50 @@
+package com.laszloborbely.jpuzzle.sudoku.rules.solution.strategy;
+
+import com.laszloborbely.jpuzzle.sudoku.matrix.QuadraticMatrix;
+import com.laszloborbely.jpuzzle.sudoku.matrix.QuadraticMatrixElement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Reduction strategy for sudoku puzzles
+ * Removes potential values from a group if there is already a fixed value existing
+ */
+public class QGroupReductionStrategy extends QReductionStrategy {
+    /**
+     * Group reducer method
+     * Removes instances of fixed values from each group of the input puzzle
+     *
+     * @param matrix Input sudoku puzzle
+     * @return True if the reduction was successful, false if not
+     */
+    @Override
+    public boolean reduce(QuadraticMatrix matrix) {
+        boolean reduced = false;
+
+        /*
+         * Iterate over each matrix group
+         */
+        for (List<QuadraticMatrixElement> group : matrix.groups()) {
+            /*
+             * Retrieve fixed Short values from group
+             */
+            List<Short> fixedList =
+                    group.stream()
+                            .filter(QuadraticMatrixElement::fixed)
+                            .map(e -> e.getValues().get(0))
+                            .collect(Collectors.toList());
+
+            /*
+             * Remove each fixed short value from other elements
+             */
+            for (Short fixed : fixedList) {
+                if (this.removeValueFromList(fixed, group)) {
+                    reduced = true;
+                }
+            }
+        }
+
+        return reduced;
+    }
+}
---
+++ src/main/java/com/laszloborbely/jpuzzle/sudoku/rules/solution/strategy/QRowReductionStrategy.java
@@ -0,0 +1,50 @@
+package com.laszloborbely.jpuzzle.sudoku.rules.solution.strategy;
+
+import com.laszloborbely.jpuzzle.sudoku.matrix.QuadraticMatrix;
+import com.laszloborbely.jpuzzle.sudoku.matrix.QuadraticMatrixElement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Reduction strategy for sudoku puzzles
+ * Removes potential values from a row if there is already a fixed value existing
+ */
+public class QRowReductionStrategy extends QReductionStrategy {
+    /**
+     * Row reducer method
+     * Removes instances of fixed values from each row of the input puzzle
+     *
+     * @param matrix Input sudoku puzzle
+     * @return True if the reduction was successful, false if not
+     */
+    @Override
+    public boolean reduce(QuadraticMatrix matrix) {
+        boolean reduced = false;
+
+        /*
+         * Iterate over each matrix row
+         */
+        for (List<QuadraticMatrixElement> row : matrix.rows()) {
+            /*
+             * Retrieve fixed Short values from row
+             */
+            List<Short> fixedList =
+                    row.stream()
+                            .filter(QuadraticMatrixElement::fixed)
+                            .map(e -> e.getValues().get(0))
+                            .collect(Collectors.toList());
+
+            /*
+             * Remove each fixed short value from other elements
+             */
+            for (Short fixed : fixedList) {
+                if (this.removeValueFromList(fixed, row)) {
+                    reduced = true;
+                }
+            }
+        }
+
+        return reduced;
+    }
+}
